d34b5e4d689d36d89eb8225449b41af29cfe5915
unknown <debzzbea@gmail.com>
2022-06-01 00:28:02 +0800

    Updated battery fuel gauge

--- battery_fuel_gauge/battery_fuel_gauge.ino
+++ battery_fuel_gauge/battery_fuel_gauge.ino
@@ -1,70 +1,43 @@
-/**
- * Need to include this library: https://github.com/adafruit/Adafruit-GFX-Library
- * This is an example demonstrating the use of the LiFuelGauge library
- * The example prints battery status information on the serial monitor
- * When an Alert Threshold interrupt is generated, the lowPower ISR is
- * called and afterwards (hypothetically) any running operations are 
- * getting finalized before the system enters sleep mode
- * 
- * Note:
- *   After exiting sleep mode or resetting, give the MAX17043/4 
- *   half a second to perform the first measurements
- */
+#include "MAX17043.h"
+#include "Wire.h"
 
-#include <Wire.h>
-#include <LiFuelGauge.h>
+MAX17043 batteryMonitor;
 
-// LiFuelGauge constructor parameters
-// 1. IC type, MAX17043 or MAX17044
-// 2. Number of interrupt to which the alert pin is associated (Optional) 
-// 3. ISR to call when an alert interrupt is generated (Optional)
-//
-// Creates a LiFuelGauge instance for the MAX17043 IC
-// and attaches lowPower to INT0 (PIN2 on most boards, PIN3 on Leonardo)
-void lowPower();
-LiFuelGauge gauge(MAX17043, 0, lowPower);
+float cellVoltage;
+float stateOfCharge;
 
-// A flag to indicate a generated alert interrupt
-volatile boolean alert = false;
+void setup() {
+  
+  Wire.begin(); 
+  Serial.begin(9600);
+  Serial.println("MAX17043 Example: reading voltage and SoC");
+  Serial.println();
+  
+  batteryMonitor.reset();
+  batteryMonitor.quickStart();
+  delay(1000);
+  
+  float cellVoltage = batteryMonitor.getVCell();
+  Serial.print("Voltage:\t\t");
+  Serial.print(cellVoltage, 4);
+  Serial.println("V");
 
-
-void setup()
-{
-    Serial.begin(9600); // Initializes serial port
-    // Waits for serial port to connect. Needed for Leonardo only
-    while ( !Serial ) ;
-    
-    gauge.reset();  // Resets MAX17043
-    delay(200);  // Waits for the initial measurements to be made
-    
-    // Sets the Alert Threshold to 10% of full capacity
-    gauge.setAlertThreshold(10);
-    Serial.println(String("Alert Threshold is set to ") + 
-                   gauge.getAlertThreshold() + '%');
+  float stateOfCharge = batteryMonitor.getSoC();
+  Serial.print("State of charge:\t");
+  Serial.print(stateOfCharge);
+  Serial.println("%");
 }
 
-void loop()
-{
-    Serial.print("SOC: ");
-    Serial.print(gauge.getSOC());  // Gets the battery's state of charge
-    Serial.print("%, VCELL: ");
-    Serial.print(gauge.getVoltage());  // Gets the battery voltage
-    Serial.println('V');
-    
-    if ( alert )
-    {
-        Serial.println("Beware, Low Power!");
-        Serial.println("Finalizing operations...");
-        gauge.clearAlertInterrupt();  // Resets the ALRT pin
-        alert = false;
-        Serial.println("Storing data...");
-        Serial.println("Sending notification...");
-        Serial.println("System operations are halted...");
-        gauge.sleep();  // Forces the MAX17043 into sleep mode
-        while ( true ) ;
-    }
-    
-    delay(2000);
-}
+void loop() {    
+  cellVoltage = batteryMonitor.getVCell();
+  Serial.print("Voltage:\t\t");
+  Serial.print(cellVoltage, 4);
+  Serial.println("V");
 
-void lowPower() { alert = true; }
+  stateOfCharge = batteryMonitor.getSoC();
+  Serial.print("State of charge:\t");
+  Serial.print(stateOfCharge);
+  Serial.println("%");
+  
+  delay(500);
+}
--- water_flow_sensor/water_flow_sensor.ino
+++ water_flow_sensor/water_flow_sensor.ino
@@ -1,117 +1,90 @@
 /*
-Liquid flow rate sensor -DIYhacking.com Arvind Sanjeev
 
-Measure the liquid/water flow rate using this code. 
-Connect Vcc and Gnd of sensor to arduino, and the 
-signal line to arduino digital pin 2.
- 
- */
+YF‐ S201 Water Flow Sensor
 
-byte statusLed    = 13;
+Water Flow Sensor output processed to read in litres/hour
 
-byte sensorInterrupt = 0;  // 0 = digital pin 2
-byte sensorPin       = 2;
+Adaptation Courtesy: hobbytronics.co.uk
 
-// The hall-effect flow sensor outputs approximately 4.5 pulses per second per
-// litre/minute of flow.
-float calibrationFactor = 4.5;
+*/
 
-volatile byte pulseCount;  
+volatile int flow_frequency; // Measures flow sensor pulses
 
-float flowRate;
-unsigned int flowMilliLitres;
-unsigned long totalMilliLitres;
+// Calculated litres/hour
 
-unsigned long oldTime;
+float vol = 0.0,l_minute;
+
+unsigned char flowsensor = 34; // Sensor Input
+
+unsigned long currentTime;
+
+unsigned long cloopTime;
+
+void flow () // Interrupt function
+
+{
+
+   flow_frequency++;
+
+}
 
 void setup()
+
 {
-  
-  // Initialize a serial connection for reporting values to the host
-  Serial.begin(9600);
-   
-  // Set up the status LED line as an output
-  pinMode(statusLed, OUTPUT);
-  digitalWrite(statusLed, HIGH);  // We have an active-low LED attached
-  
-  pinMode(sensorPin, INPUT);
-  digitalWrite(sensorPin, HIGH);
 
-  pulseCount        = 0;
-  flowRate          = 0.0;
-  flowMilliLitres   = 0;
-  totalMilliLitres  = 0;
-  oldTime           = 0;
+   pinMode(flowsensor, INPUT);
 
-  // The Hall-effect sensor is connected to pin 2 which uses interrupt 0.
-  // Configured to trigger on a FALLING state change (transition from HIGH
-  // state to LOW state)
-  attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+   digitalWrite(flowsensor, HIGH); // Optional Internal Pull-Up
+   digitalWrite(32, HIGH);
+
+   Serial.begin(9600);
+
+   attachInterrupt(digitalPinToInterrupt(flowsensor), flow, RISING); // Setup Interrupt
+
+   currentTime = millis();
+
+   cloopTime = currentTime;
+
 }
 
-/**
- * Main program loop
- */
-void loop()
+void loop ()
+
 {
-   
-   if((millis() - oldTime) > 1000)    // Only process counters once per second
-  { 
-    // Disable the interrupt while calculating flow rate and sending the value to
-    // the host
-    detachInterrupt(sensorInterrupt);
-        
-    // Because this loop may not complete in exactly 1 second intervals we calculate
-    // the number of milliseconds that have passed since the last execution and use
-    // that to scale the output. We also apply the calibrationFactor to scale the output
-    // based on the number of pulses per second per units of measure (litres/minute in
-    // this case) coming from the sensor.
-    flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
-    
-    // Note the time this processing pass was executed. Note that because we've
-    // disabled interrupts the millis() function won't actually be incrementing right
-    // at this point, but it will still return the value it was set to just before
-    // interrupts went away.
-    oldTime = millis();
-    
-    // Divide the flow rate in litres/minute by 60 to determine how many litres have
-    // passed through the sensor in this 1 second interval, then multiply by 1000 to
-    // convert to millilitres.
-    flowMilliLitres = (flowRate / 60) * 1000;
-    
-    // Add the millilitres passed in this second to the cumulative total
-    totalMilliLitres += flowMilliLitres;
-      
-    unsigned int frac;
-    
-    // Print the flow rate for this second in litres / minute
-    Serial.print("Flow rate: ");
-    Serial.print(int(flowRate));  // Print the integer part of the variable
-    Serial.print("L/min");
-    Serial.print("\t");       // Print tab space
 
-    // Print the cumulative total of litres flowed since starting
-    Serial.print("Output Liquid Quantity: ");        
-    Serial.print(totalMilliLitres);
-    Serial.println("mL"); 
-    Serial.print("\t");       // Print tab space
-    Serial.print(totalMilliLitres/1000);
-    Serial.print("L");
-    
+   currentTime = millis();
 
-    // Reset the pulse counter so we can start incrementing again
-    pulseCount = 0;
-    
-    // Enable the interrupt again now that we've finished sending output
-    attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
-  }
-}
+   // Every second, calculate and print litres/hour
 
-/*
-Insterrupt Service Routine
- */
-void pulseCounter()
-{
-  // Increment the pulse counter
-  pulseCount++;
+   if(currentTime >= (cloopTime + 1000))
+
+   {
+
+    cloopTime = currentTime; // Updates cloopTime
+
+    if(flow_frequency != 0){
+
+      // Pulse frequency (Hz) = 7.5Q, Q is flow rate in L/min.
+
+      l_minute = (flow_frequency / 7.5); // (Pulse frequency x 60 min) / 7.5Q = flowrate in L/hour
+
+      l_minute = l_minute/60;
+
+      vol = vol +l_minute;
+
+      flow_frequency = 0; // Reset Counter
+
+      Serial.print(l_minute, DEC); // Print litres/hour
+
+      Serial.println(" L/Sec");
+
+    }
+
+    else {
+
+      Serial.println(" flow rate = 0 ");
+
+    }
+
+   }
+
 }
